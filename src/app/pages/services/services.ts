b4222c0df133c54da8bdccc41c1d660c72478def
Fahad Hussain Chandio <fchandio07@gmail.com>
2026-03-30 13:03:44 +0500

    Moved Specialized Services to dedicated Services page; cleaned home page

--- src/app/pages/services/services.ts
+++ src/app/pages/services/services.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Services as ServicesSection } from '../../sections/services/services';
 
 @Component({
   selector: 'app-services-page',
   standalone: true,
-  imports: [CommonModule, ServicesSection],
-  template: `<app-services style="display: block; padding-top: 100px;"></app-services>`
+  imports: [CommonModule],
+  templateUrl: './services.html',
+  styleUrl: './services.scss'
 })
 export class Services {}
